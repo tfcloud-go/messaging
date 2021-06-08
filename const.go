bc3f8508b79f8677f050d8d4e52466285a283008
William Huang <ai.william@outlook.com>
2021-06-08 15:01:50 +0800

    add channel type (#2)
    Signed-off-by: William Huang <ai.william@outlook.com>

---
+++ const.go
@@ -0,0 +1,12 @@
+// Copyright 2021 TFCloud Co.,Ltd. All rights reserved.
+// This source code is licensed under Apache-2.0 license
+// that can be found in the LICENSE file.
+
+package messaging
+
+type ChannelType string
+
+const (
+	SMS   ChannelType = "sms"
+	Email ChannelType = "email"
+)
